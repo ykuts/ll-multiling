65adab7858aed02cef2f99c55c913f4f6b7f59a9
Yuliia <yuliiakuts@gmail.com>
2025-05-28 22:42:00 +0200

    Fixed deploy configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Полностью отключаем оптимизацию изображений для статического хостинга
+  images: {
+    unoptimized: true
+  },
+  // Добавляем настройки для правильной работы на Netlify
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out'
 };
 
 export default nextConfig;
