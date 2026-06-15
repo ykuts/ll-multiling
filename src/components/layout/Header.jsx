bd75e7c51fbd08f514fc83432f374f8c5ad90f9a
Yuliia <yuliiakuts@gmail.com>
2026-06-15 13:51:58 +0200

    feat: step1 - tagline, footer, navigation updates

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -43,6 +43,7 @@ const Header = () => {
         }
       ]
     },
+    { name: t('navigation.project1000'), href: '/project1000/' },
     { name: t('navigation.credentials'), href: '/credentials/' },
   ];
 
